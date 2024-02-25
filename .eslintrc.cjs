812e311e2b54f37f2d236899b48082706dbd887d
rishiraj-dhar <rishiraj1203@gmail.com>
2024-02-25 18:21:16 +0530

    add eslint-plugin-react to project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
