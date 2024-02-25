5ef96d01d78b38ad98e7aff775d7e4c7eccde459
rishiraj-dhar <rishiraj1203@gmail.com>
2024-02-26 02:06:55 +0530

    add prettier to project

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  experimentalTernaries: true,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+};
+
+module.exports = config;
