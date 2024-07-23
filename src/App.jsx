f26d84232171de3e74f734f9375337fa910d57df
rishiraj-dhar <rishiraj1203@gmail.com>
2024-07-23 17:14:32 +0530

    style: add css and assets to html skeleton
    Add basic styling and assets to build the layout of the app.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import greekSaladImage from "src/assets/greek-salad.jpg";
 import heroImage from "src/assets/hero-image.jpg";
 import lemonDessertImage from "src/assets/lemon-dessert.jpg";
 
+import sarahJohnsonImage from "src/assets/sarah-johnson.jpg";
+import michaelThompsonImage from "src/assets/michael-thompson.jpg";
+import emilyDavisImage from "src/assets/emily-davis.jpg";
+
+import "./App.css";
+
 const specials = [
   {
     name: "Greek Salad",
@@ -26,168 +32,234 @@ const specials = [
   },
 ];
 
-function App() {
+const testimonials = [
+  {
+    img: sarahJohnsonImage,
+    name: "Sarah Johnson",
+    rating: 5,
+    review:
+      "Little Lemon is a gem in Chicago! The salads are a perfect blend of flavors and textures, and the cheese is simply to die for. The ambiance is warm and welcoming, making it a great spot for family dinners. Highly recommend!",
+  },
+  {
+    img: michaelThompsonImage,
+    name: "Michael Thompson",
+    rating: 4,
+    review:
+      "I had a wonderful experience at Little Lemon. The appetizers were crispy and flavorful, just like how they should be. The modern twist on traditional Mediterranean dishes is a delightful touch. My only complaint is that it gets a bit crowded on weekends, but it's worth the wait!",
+  },
+  {
+    img: emilyDavisImage,
+    name: "Emily Davis",
+    rating: 5,
+    review:
+      "Little Lemon's desserts are an absolute must-try! They reminded me of my grandmother's desserts, so authentic and delicious. The staff is friendly, and the service is excellent. I can't wait to come back and try more dishes!",
+  },
+];
+
+function Header() {
   return (
-    <>
-      {/* Navigation bar */}
-      <header>
-        <span>
+    <header className="outer-wrapper">
+      <div className="inner-wrapper">
+        <span className="header-logo">
           <img src="/little-lemon-wordmark.svg" alt="Little Lemon Logo" />
         </span>
         <nav>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Menu</li>
-            <li>Reservations</li>
-            <li>Order online</li>
-            <li>Login</li>
+            <li>
+              <a className="nav-link" href="#">
+                <span>Home</span>
+              </a>
+            </li>
+            <li>
+              <a className="nav-link" href="#">
+                <span>About</span>
+              </a>
+            </li>
+            <li>
+              <a className="nav-link" href="#">
+                <span>Menu</span>
+              </a>
+            </li>
+            <li>
+              <a className="nav-link" href="#">
+                <span>Reservations</span>
+              </a>
+            </li>
+            <li>
+              <a className="nav-link" href="#">
+                <span>Order online</span>
+              </a>
+            </li>
+            <li>
+              <a className="nav-link" href="#">
+                <span>Login</span>
+              </a>
+            </li>
           </ul>
         </nav>
-      </header>
+      </div>
+    </header>
+  );
+}
+
+function SpecialCard({ special }) {
+  return (
+    <article className="special-card">
+      <div className="special-card-content">
+        <hgroup className="special-card-header">
+          <h3 className="special-card-name">{special.name}</h3>
+          <p className="special-card-price">{special.price}</p>
+        </hgroup>
+        <p className="special-card-copy">{special.desc}</p>
+        <Button className="special-card-cta">Order a delivery</Button>
+      </div>
+      <div className="special-card-image">
+        <img src={special.img} alt={"Image of " + special.name} />
+      </div>
+    </article>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Header />
 
       {/* Hero Section */}
-      <section>
-        <div>
-          <hgroup>
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
+      <section className="hero outer-wrapper">
+        <div className="inner-wrapper">
+          <hgroup className="hero-header">
+            <h1 className="hero-title">Little Lemon</h1>
+            <h2 className="hero-subtitle">Chicago</h2>
           </hgroup>
-          <p>
-            We are a family owned Mediterranean restaurant, focused on
-            traditional recipes served with a modern twist.
-          </p>
-          <Button>Reserve a Table</Button>
-        </div>
-        <div>
-          <img src={heroImage} alt="Hero Image" />
+          <div className="hero-content">
+            <p className="hero-copy">
+              We are a family owned Mediterranean restaurant, focused on
+              traditional recipes served with a modern twist.
+            </p>
+            <Button className="hero-cta">Reserve a Table</Button>
+          </div>
+          <div className="hero-image">
+            <img src={heroImage} alt="Hero Image" />
+          </div>
         </div>
       </section>
 
       {/* Highlights and specials */}
-      <section>
-        <hgroup>
-          <h2>Specials</h2>
-          <Button>Online Menu</Button>
-        </hgroup>
-        <div>
-          {specials.map((special, index) => (
-            <article key={index}>
-              <div>
-                <hgroup>
-                  <h3>{special.name}</h3>
-                  <p>{special.price}</p>
-                </hgroup>
-                <p>{special.desc}</p>
-                <Button>Order a delivery</Button>
-              </div>
-              <div>
-                <img src={special.img} alt={"Image of " + special.name} />
-              </div>
-            </article>
-          ))}
+      <section className="specials outer-wrapper">
+        <div className="inner-wrapper">
+          <hgroup>
+            <div className="specials-header-row inner-wrapper">
+              <h2 className="header">Specials</h2>
+              <Button className="specials-cta">Online Menu</Button>
+            </div>
+          </hgroup>
+          <div className="specials-item-row">
+            {specials.map((special) => (
+              <SpecialCard key={special.name} special={special} />
+            ))}
+          </div>
         </div>
       </section>
 
       {/* Testimonials */}
-      <section>
-        <h2>Testimonials</h2>
-        <div>
-          <article>
-            <h3>Rating 01</h3>
-            <div>
-              <span>Image here</span>
-              <span>Name 01</span>
-            </div>
-            <p>
-              Review goes here... Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat.
-            </p>
-          </article>
-          <article>
-            <h3>Rating 02</h3>
-            <div>
-              <span>Image here</span>
-              <span>Name 02</span>
-            </div>
-            <p>
-              Review goes here... Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat.
-            </p>
-          </article>
-          <article>
-            <h3>Rating 03</h3>
-            <div>
-              <span>Image here</span>
-              <span>Name 03</span>
-            </div>
-            <p>
-              Review goes here... Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat.
-            </p>
-          </article>
+      <section className="testimonials outer-wrapper">
+        <div className="inner-wrapper">
+          <h2 className="header">Testimonials</h2>
+          <div className="testimonial-row">
+            {testimonials.map((testimonial) => (
+              <article className="testimonial" key={testimonial.name}>
+                <div className="testimonial-contributor-image">
+                  <img
+                    src={testimonial.img}
+                    alt={"Photo of " + testimonial.name}
+                  />
+                </div>
+                <div className="testimonial-contributor-name">
+                  {testimonial.name}
+                </div>
+                <div className="testimonial-rating">
+                  {testimonial.rating}
+                  {"⭐".repeat(testimonial.rating)}
+                </div>
+                <p className="testimonial-review">{testimonial.review}</p>
+              </article>
+            ))}
+          </div>
         </div>
       </section>
 
       {/* About Little Lemon */}
-      <section>
-        <div>
-          <hgroup>
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-          </hgroup>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+      <section className="about outer-wrapper">
+        <div className="inner-wrapper">
+          <div>
+            <hgroup className="about-header">
+              <h1 className="about-title">Little Lemon</h1>
+              <h2 className="about-subtitle">Chicago</h2>
+            </hgroup>
+            <p>
+              We are a family owned Mediterranean restaurant, focused on
+              traditional recipes served with a modern twist.
+            </p>
+          </div>
+          <div>{/* About Image */}</div>
         </div>
-        <div>{/* About Image */}</div>
       </section>
 
       {/* Footer */}
-      <footer>
-        <div>
-          <img src="/little-lemon-wordmark.svg" alt="Little Lemon Logo" />
-        </div>
-        <div>
+      <footer className="outer-wrapper">
+        <div className="inner-wrapper">
+          <div className="footer-logo">
+            <img src="/little-lemon-wordmark.svg" alt="Little Lemon Logo" />
+          </div>
           <div>
-            <h3>Doormat Links</h3>
+            <h3 className="footer-column-header">Doormat Links</h3>
             <nav>
               <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Reservations</li>
-                <li>Order online</li>
-                <li>Login</li>
+                <li className="footer-column-item">
+                  <a className="nav-link" href="#">
+                    <span>Home</span>
+                  </a>
+                </li>
+                <li className="footer-column-item">
+                  <a className="nav-link" href="#">
+                    <span>About</span>
+                  </a>
+                </li>
+                <li className="footer-column-item">
+                  <a className="nav-link" href="#">
+                    <span>Menu</span>
+                  </a>
+                </li>
+                <li className="footer-column-item">
+                  <a className="nav-link" href="#">
+                    <span>Reservations</span>
+                  </a>
+                </li>
+                <li className="footer-column-item">
+                  <a className="nav-link" href="#">
+                    <span>Order online</span>
+                  </a>
+                </li>
+                <li className="footer-column-item">
+                  <a className="nav-link" href="#">
+                    <span>Login</span>
+                  </a>
+                </li>
               </ul>
             </nav>
           </div>
           <div>
-            <h3>Contact Us</h3>
-            <p>Address</p>
-            <p>Phone Number</p>
-            <p>Email</p>
+            <h3 className="footer-column-header">Contact Us</h3>
+            <p className="footer-column-item">Address</p>
+            <p className="footer-column-item">Phone Number</p>
+            <p className="footer-column-item">Email</p>
           </div>
           <div>
-            <h3>Social Media Links</h3>
-            <p>Facebook</p>
-            <p>Instagram</p>
-            <p>X</p>
+            <h3 className="footer-column-header">Social Media Links</h3>
+            <p className="footer-column-item">Facebook</p>
+            <p className="footer-column-item">Instagram</p>
+            <p className="footer-column-item">X</p>
           </div>
         </div>
       </footer>
