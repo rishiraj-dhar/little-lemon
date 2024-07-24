ccf5d749ce56a48710a2a8644a61e27af64109ec
rishiraj-dhar <rishiraj1203@gmail.com>
2024-07-24 14:06:21 +0530

    feat: add client-side routing
    Set up router and render the landing page at the root index route.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./main.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/home-page.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
