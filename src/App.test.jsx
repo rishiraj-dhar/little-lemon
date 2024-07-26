de78f0842569c3c73c533cf77ad72f46594f49b8
rishiraj-dhar <rishiraj1203@gmail.com>
2024-07-27 01:43:28 +0530

    chore: remove obsolete test

--- src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom/vitest";
-import { render, screen } from "@testing-library/react";
-import { expect, it } from "vitest";
-import App from "./App";
-
-it("renders `<App />` correctly", () => {
-  render(<App />);
-  expect(screen.getByRole("heading")).toHaveTextContent("Little Lemon");
-});
